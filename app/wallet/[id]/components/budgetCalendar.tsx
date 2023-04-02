163867f50aff6fcd228e0b77c3ccb985c1d051b4
rcrosbourne <rcrosbourne@gmail.com>
2023-04-02 14:17:36 -0500

    feat: added popover with collision detection to display the days transactions

--- app/wallet/[id]/components/budgetCalendar.tsx
+++ app/wallet/[id]/components/budgetCalendar.tsx
@@ -1,27 +1,30 @@
 import React, { Fragment } from "react";
 import {
+  AdjustmentsHorizontalIcon,
   ChevronDownIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   EllipsisHorizontalIcon,
+  XMarkIcon,
 } from "@heroicons/react/20/solid";
 import { Menu, Transition } from "@headlessui/react";
-import {classNames, formatNumberAsCurrency} from "@/app/utils";
+import { classNames, formatNumberAsCurrency } from "@/app/utils";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import dayjs from "dayjs";
 import { FinancialTransaction } from "@/app/types/financialTransaction";
-
+import * as Popover from "@radix-ui/react-popover";
+import { Close } from "@radix-ui/react-popover";
 type CalendarDay = {
-    date: string;
-    transactions: FinancialTransaction[];
-    isCurrentMonth: boolean;
-    isToday: boolean;
-    isSelected: boolean;
-}
+  date: string;
+  transactions: FinancialTransaction[];
+  isCurrentMonth: boolean;
+  isToday: boolean;
+  isSelected: boolean;
+};
 type Calendar = {
-    month: string;
-    days: CalendarDay[];
-}
+  month: string;
+  days: CalendarDay[];
+};
 const generateCalendarFromMonth = (
   month: string,
   transactions: FinancialTransaction[]
@@ -30,7 +33,7 @@ const generateCalendarFromMonth = (
   const lastDayOfMonth = dayjs(month).endOf("month").toDate();
   const firstDayOfCalendar = dayjs(firstDayOfMonth).startOf("week").toDate();
   const lastDayOfCalendar = dayjs(lastDayOfMonth).endOf("week").toDate();
-  const calendar:CalendarDay[] = [];
+  const calendar: CalendarDay[] = [];
   let day = firstDayOfCalendar;
   while (day <= lastDayOfCalendar) {
     // if there are transactions for the day, add them to the calendar
@@ -64,7 +67,11 @@ const generateCalendarFromMonth = (
   }
   return { month: dayjs(month).format("MMMM YYYY"), days: calendar };
 };
-const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) => {
+const BudgetCalendar = ({
+  transactions,
+}: {
+  transactions: FinancialTransaction[];
+}) => {
   const [currentMonth, setCurrentMonth] = React.useState(
     dayjs().format("YYYY-MM-DD")
   );
@@ -78,9 +85,14 @@ const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) 
       isSelected: boolean;
     }[]
   >([]);
-  const [selectedDay, setSelectedDay] = React.useState<CalendarDay|undefined>(undefined);
+  const [selectedDay, setSelectedDay] = React.useState<CalendarDay | undefined>(
+    undefined
+  );
   React.useEffect(() => {
-    const { month, days } = generateCalendarFromMonth(currentMonth, transactions);
+    const { month, days } = generateCalendarFromMonth(
+      currentMonth,
+      transactions
+    );
     setMonth(month);
     setDays(days);
     setSelectedDay(days.find((day) => day.isSelected));
@@ -100,7 +112,11 @@ const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) 
             />
             <button
               type="button"
-              onClick={() => setCurrentMonth(dayjs(currentMonth).subtract(1, "month").toISOString())}
+              onClick={() =>
+                setCurrentMonth(
+                  dayjs(currentMonth).subtract(1, "month").toISOString()
+                )
+              }
               className="flex items-center justify-center rounded-l-md py-2 pl-3 pr-4 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50"
             >
               <span className="sr-only">Previous month</span>
@@ -116,7 +132,11 @@ const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) 
             <span className="relative -mx-px h-5 w-px bg-gray-300 md:hidden" />
             <button
               type="button"
-              onClick={() => setCurrentMonth(dayjs(currentMonth).add(1, "month").toISOString())}
+              onClick={() =>
+                setCurrentMonth(
+                  dayjs(currentMonth).add(1, "month").toISOString()
+                )
+              }
               className="flex items-center justify-center rounded-r-md py-2 pl-4 pr-3 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50"
             >
               <span className="sr-only">Next month</span>
@@ -212,16 +232,51 @@ const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) 
                     <ol className="mt-2">
                       {day.transactions.slice(0, 2).map((transaction) => (
                         <li key={transaction.id}>
-                          <button type="button" className="group flex w-full">
-                            <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
-                              {transaction.category?.name}
-                            </p>
-                            <p
-                              className="ml-3 hidden flex-none text-gray-500 group-hover:text-indigo-600 xl:block"
-                            >
-                              {"$" + formatNumberAsCurrency(transaction.amount)}
-                            </p>
-                          </button>
+                          <Popover.Root>
+                            <Popover.Trigger asChild>
+                              <button className="group flex w-full">
+                                <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
+                                  {transaction.category?.name}
+                                </p>
+                                <p className="ml-3 hidden flex-none text-gray-500 group-hover:text-indigo-600 xl:block">
+                                  {"$" +
+                                    formatNumberAsCurrency(transaction.amount)}
+                                </p>
+                              </button>
+                            </Popover.Trigger>
+
+                            <Popover.Portal>
+                              <Popover.Content
+                                className="rounded-lg p-5 w-[260px] bg-slate-100 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                                sideOffset={5}
+                              >
+                                <div className="overflow-hidden">
+                                  <div className="relative grid gap-8 bg-slate-100 p-3 lg:grid-cols-1">
+                                    {day.transactions.map((item) => (
+                                      <a
+                                        key={item.id}
+                                        href="#"
+                                        className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-slate-50 focus:outline-none focus-visible:ring focus-visible:ring-cyan-500 focus-visible:ring-opacity-50"
+                                      >
+                                        <div className={`flex h-10 w-10 px-2 rounded-full ${item.category?.backgroundColor} ${item.category?.foregroundColor} shrink-0 items-center justify-center sm:h-10 sm:w-10`}>
+                                          {item.category?.icon}
+                                        </div>
+                                        <div className="ml-4">
+                                          <p className="text-sm font-medium text-gray-900">
+                                            {"$" + formatNumberAsCurrency(item.amount)}
+                                          </p>
+                                          <p className="text-sm text-gray-500">
+                                            {item.category?.name}
+                                          </p>
+                                        </div>
+                                      </a>
+                                    ))}
+                                  </div>
+                                </div>
+                                <Popover.Arrow className="fill-slate-100" />
+                              </Popover.Content>
+                            </Popover.Portal>
+                          </Popover.Root>
                         </li>
                       ))}
                       {day.transactions.length > 2 && (
@@ -314,7 +369,10 @@ const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) 
                   href="#"
                   className="ml-6 flex-none self-center rounded-md bg-white px-3 py-2 font-semibold text-gray-900 opacity-0 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400 focus:opacity-100 group-hover:opacity-100"
                 >
-                  Edit<span className="sr-only">, ${formatNumberAsCurrency(transaction.amount)}</span>
+                  Edit
+                  <span className="sr-only">
+                    , ${formatNumberAsCurrency(transaction.amount)}
+                  </span>
                 </a>
               </li>
             ))}
@@ -322,6 +380,80 @@ const BudgetCalendar = ({transactions}: {transactions: FinancialTransaction[]}) 
         </div>
       )}
     </div>
+  );
+};
+const PopoverPanel = ({
+  transactions,
+}: {
+  transactions: FinancialTransaction[];
+}) => {
+  return (
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={`
+                ${open ? "" : "text-opacity-90"}
+                group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+          >
+            <span>Solutions</span>
+            <ChevronDownIcon
+              className={`${open ? "" : "text-opacity-70"}
+                  ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
+              aria-hidden="true"
+            />
+          </Popover.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                  {transactions.map((item) => (
+                    <a
+                      key={item.id}
+                      href="#"
+                      className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                    >
+                      <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                        {item.category?.icon}
+                      </div>
+                      <div className="ml-4">
+                        <p className="text-sm font-medium text-gray-900">
+                          {item.category?.name}
+                        </p>
+                        <p className="text-sm text-gray-500">{item.merchant}</p>
+                      </div>
+                    </a>
+                  ))}
+                </div>
+                <div className="bg-gray-50 p-4">
+                  <a
+                    href="##"
+                    className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                  >
+                    <span className="flex items-center">
+                      <span className="text-sm font-medium text-gray-900">
+                        Documentation
+                      </span>
+                    </span>
+                    <span className="block text-sm text-gray-500">
+                      Start integrating products and tools
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
   );
 };
 export default BudgetCalendar;
