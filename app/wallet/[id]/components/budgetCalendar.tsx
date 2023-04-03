88242805cee9d7c3cac0ebb7ead2268bf8765c12
rcrosbourne <rcrosbourne@gmail.com>
2023-04-02 20:30:09 -0500

    feat: clean up type errors

--- app/wallet/[id]/components/budgetCalendar.tsx
+++ app/wallet/[id]/components/budgetCalendar.tsx
@@ -222,7 +222,7 @@ const BudgetCalendar = ({
                         : undefined
                     }
                   >
-                    {day.date.split("-").pop().replace(/^0/, "")}
+                    {day?.date?.split("-")?.pop()?.replace(/^0/, "")}
                   </time>
                   {day.transactions.length > 0 && (
                     <ol className="mt-2">
@@ -293,31 +293,28 @@ const BudgetCalendar = ({
                   type="button"
                   className={classNames(
                     day.isCurrentMonth ? "bg-white" : "bg-gray-50",
-                    (day.isSelected || day.isToday) && "font-semibold",
-                    day.isSelected && "text-white",
-                    !day.isSelected && day.isToday && "text-indigo-600",
+                    (day.isSelected || day.isToday) ? "font-semibold" : "",
+                    day.isSelected ? "text-white" : "",
+                    !day.isSelected && day.isToday ? "text-indigo-600" : "",
                     !day.isSelected &&
                       day.isCurrentMonth &&
-                      !day.isToday &&
-                      "text-gray-900",
+                      !day.isToday ? "text-gray-900" : "",
                     !day.isSelected &&
                       !day.isCurrentMonth &&
-                      !day.isToday &&
-                      "text-gray-500",
+                      !day.isToday ? "text-gray-500" : "",
                     "flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10"
                   )}
                 >
                   <time
                     dateTime={day.date}
                     className={classNames(
-                      day.isSelected &&
-                        "flex h-6 w-6 items-center justify-center rounded-full",
-                      day.isSelected && day.isToday && "bg-indigo-600",
-                      day.isSelected && !day.isToday && "bg-gray-900",
+                      day.isSelected ? "flex h-6 w-6 items-center justify-center rounded-full" : "",
+                      day.isSelected && day.isToday ? "bg-indigo-600" : "",
+                      day.isSelected && !day.isToday ? "bg-gray-900" : "",
                       "ml-auto"
                     )}
                   >
-                    {day.date.split("-").pop().replace(/^0/, "")}
+                    {day?.date?.split("-")?.pop()?.replace(/^0/, "")}
                   </time>
                   <span className="sr-only">
                     {day.transactions.length} transactions
@@ -376,80 +373,6 @@ const BudgetCalendar = ({
         </div>
       )}
     </div>
-  );
-};
-const PopoverPanel = ({
-  transactions,
-}: {
-  transactions: FinancialTransaction[];
-}) => {
-  return (
-    <Popover className="relative">
-      {({ open }) => (
-        <>
-          <Popover.Button
-            className={`
-                ${open ? "" : "text-opacity-90"}
-                group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
-            <span>Solutions</span>
-            <ChevronDownIcon
-              className={`${open ? "" : "text-opacity-70"}
-                  ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
-              aria-hidden="true"
-            />
-          </Popover.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
-              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
-                  {transactions.map((item) => (
-                    <a
-                      key={item.id}
-                      href="#"
-                      className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                    >
-                      <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                        {item.category?.icon}
-                      </div>
-                      <div className="ml-4">
-                        <p className="text-sm font-medium text-gray-900">
-                          {item.category?.name}
-                        </p>
-                        <p className="text-sm text-gray-500">{item.merchant}</p>
-                      </div>
-                    </a>
-                  ))}
-                </div>
-                <div className="bg-gray-50 p-4">
-                  <a
-                    href="##"
-                    className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                  >
-                    <span className="flex items-center">
-                      <span className="text-sm font-medium text-gray-900">
-                        Documentation
-                      </span>
-                    </span>
-                    <span className="block text-sm text-gray-500">
-                      Start integrating products and tools
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
-    </Popover>
   );
 };
 export default BudgetCalendar;
