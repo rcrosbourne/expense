e25b722888906d8db90b30cd5c0f9309fe5cc347
rcrosbourne <rcrosbourne@gmail.com>
2023-04-11 04:57:20 -0500

    fix: update classes to use hsl values instead of the misconfigured theme value

--- app/wallet/[id]/components/budgetCalendar.tsx
+++ app/wallet/[id]/components/budgetCalendar.tsx
@@ -242,7 +242,7 @@ const BudgetCalendar = ({
 
                             <Popover.Portal>
                               <Popover.Content
-                                className="rounded-lg p-5 w-[260px] bg-slate-100 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                                className="rounded-lg p-5 w-[260px] bg-slate-100 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_hsl(252, 71.0%, 83.7%)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
                                 sideOffset={5}
                               >
                                 <div className="overflow-hidden">
