776a55e156f3bb376c0831b66a0374c08e834f7f
rcrosbourne <rcrosbourne@gmail.com>
2023-04-17 04:50:07 -0500

    refactor: add utility function to capitalize a string

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -2,3 +2,4 @@ export *  from "./classNames";
 export *  from "./formatNumberAsCurrency";
 export *  from "./convertHslToTailwindArbitraryValue";
 export * from "./addAlphaToHsl";
+export * from "./capitalize";
