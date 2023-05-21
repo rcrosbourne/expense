5269cba7c2906a31aa7f86ef46dcd531c6ec89a0
rcrosbourne <rcrosbourne@gmail.com>
2023-05-21 08:52:39 -0500

    refactor: remove mock user

--- app/data/user.ts
+++
@@ -1,8 +0,0 @@
-import profilePic from "@/app/assets/images/profilePic.jpg"
-import {User} from "@/app/types";
-export const user: User = {
-  name: "Daniela Crosbourne",
-  email: "daniela.crosbourne@example.com",
-  role: "Underwriting Manager",
-  image: profilePic,
-};
